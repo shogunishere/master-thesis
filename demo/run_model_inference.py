Fixed project_dir path issues and image scaling with adaptive width inference.

demo/run_model_inference.py
@@ -15,6 +15,7 @@ from torchmetrics.classification import (
     BinaryF1Score,
     BinaryJaccardIndex,
 )
+from torchvision.transforms import transforms
 
 from andraz import settings
 from andraz.data.data import ImageImporter
@@ -44,7 +45,6 @@ class Inference:
         self.model = self._load_test_model(model)
         self.model.eval()
         self.image_resolution = image_resolution
-        self.test_loader = self._load_test_data()
         self.metrics = METRICS if metrics is None else metrics
         self.results = {
             x: {
@@ -60,25 +60,27 @@ class Inference:
 
     @staticmethod
     def _load_test_model(model):
-        return torch.load(Path(settings.PROJECT_DIR) / "training/garage/" / model)
+        return torch.load(
+            Path(settings.PROJECT_DIR) / "andraz/training/garage/" / model
+        )
 
-    def _load_test_data(self):
+    def _load_test_data(self, smaller):
         ii = ImageImporter(
             "cofly",
             only_test=True,
-            smaller=self.image_resolution,
+            smaller=smaller,
         )
         _, test = ii.get_dataset()
         return DataLoader(test, batch_size=1, shuffle=False)
 
     def _infer(self):
-
+        # Infer for all available widths of the model
         with torch.no_grad():
+            test_loader = self._load_test_data(self.image_resolution)
             for width in settings.WIDTHS:
                 self.model.set_width(width)
-
                 i = 1
-                for X, y in self.test_loader:
+                for X, y in test_loader:
                     X = X.to("cuda:0")
                     y = y.to("cuda:0")
                     y_pred = self.model.forward(X)
@@ -100,13 +102,21 @@ class Inference:
                                 .to(self.device)(y[0][j], y_pred[0][j])
                                 .cpu()
                             )
+        # Infer with the adaptation algorithm
         with torch.no_grad():
+            # We load the test loader again, as we need full-size
+            # images in order for the width selection to work properly
+            test_loader = self._load_test_data(False)
+            smaller = transforms.Resize(self.image_resolution)
             i = 1
-            for X, y in self.test_loader:
+            for X, y in test_loader:
                 # Get the image width and set the model to it
                 image = self.tensor_to_image(X)[0]
                 width = self.width_selection.get_image_width(image)
                 self.model.set_width(width)
+                # Resize the image to the required input size
+                X = smaller(X)
+                y = smaller(y)
 
                 X = X.to("cuda:0")
                 y = y.to("cuda:0")
@@ -118,7 +128,7 @@ class Inference:
                         X[0].permute(1, 2, 0).cpu(),
                         y[0][1].cpu(),
                         y_pred[0][1].cpu(),
-                        int(width * 100),
+                        "adapt",
                         i,
                     )
 

ioana/labels.py
@@ -16,6 +16,7 @@ from image_processing.vegetation_features import VegetationIndices
 
 METRICS = {"precision": BinaryPrecision}
 
+
 class Labels:
     def __init__(self, model, metrics=None, image_resolution=None):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -33,19 +34,21 @@ class Labels:
             "train": {
                 width: {
                     "precision": {"weeds": [], "back": []},
-                } for width in settings.WIDTHS
+                }
+                for width in settings.WIDTHS
             },
             "test": {
                 width: {
                     "precision": {"weeds": [], "back": []},
-                } for width in settings.WIDTHS
+                }
+                for width in settings.WIDTHS
             },
         }
 
     @staticmethod
     def _load_model(model):
-        return torch.load(Path(settings.PROJECT_DIR) / "/home/agriadapt/agriadapt/andraz/training/garage/" / model)
-    
+        return torch.load(Path(settings.PROJECT_DIR) / "andraz/training/garage" / model)
+
     def _load_data(self):
         ii = ImageImporter(
             "cofly",
@@ -55,16 +58,24 @@ class Labels:
         train, test = ii.get_dataset()
 
         return train, test
-    
+
     def _load_features(self):
-        train_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/train_features.pickle')
-        test_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/test_features.pickle')
+        train_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/train_features.pickle"
+        )
+        test_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/test_features.pickle"
+        )
 
         return train_features, test_features
-    
+
     def _filter_data(self):
-        train_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/train_features.pickle')
-        test_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/test_features.pickle')
+        train_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/train_features.pickle"
+        )
+        test_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/test_features.pickle"
+        )
         train_indices = train_features["index"].tolist()
         test_indices = test_features["index"].tolist()
         filtered_train = []
@@ -74,21 +85,17 @@ class Labels:
             if elem in train_indices:
                 sample = self.train[elem]
                 filtered_train.append(sample)
-        
+
         for elem in range(len(self.test)):
             if elem in test_indices:
                 sample = self.test[elem]
                 filtered_test.append(sample)
 
-        train_data_loader = DataLoader(
-            filtered_train, batch_size=1, shuffle=False
-        )
-        test_data_loader = DataLoader(
-            filtered_test, batch_size=1, shuffle=False
-        )
+        train_data_loader = DataLoader(filtered_train, batch_size=1, shuffle=False)
+        test_data_loader = DataLoader(filtered_test, batch_size=1, shuffle=False)
 
         return train_data_loader, test_data_loader
-    
+
     def _infer(self, width):
         with torch.no_grad():
             self.model.set_width(width)
@@ -113,30 +120,41 @@ class Labels:
                                 .cpu()
                             )
 
-        return self.results["train"][width]["precision"]["weeds"], self.results["test"][width]["precision"]["weeds"]
-    
+        return (
+            self.results["train"][width]["precision"]["weeds"],
+            self.results["test"][width]["precision"]["weeds"],
+        )
+
     def _calculate_precision_mean(self, precision_scores, draw_graph=False):
         numeric_values = [value.item() for value in precision_scores]
         boxplot = plt.boxplot(numeric_values, vert=False)
-        mean = "%.3f"% (boxplot['medians'][0].get_xdata()[0])
+        mean = "%.3f" % (boxplot["medians"][0].get_xdata()[0])
 
         if draw_graph:
             plt.figure()
-            plt.title(f"Precision Score Distribution for 'weeds' {self.width} model {self.model_name}")
+            plt.title(
+                f"Precision Score Distribution for 'weeds' {self.width} model {self.model_name}"
+            )
             plt.xlabel("Precision Score")
             plt.ylabel("Class: weeds")
             plt.show()
 
         return mean
-    
-    def _compute_labels(self, width, precision_list_train, precision_list_test, save_df=False):
-        train_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/train_features.pickle')
-        test_features = pd.read_pickle('/home/agriadapt/agriadapt/ioana/test_features.pickle')
+
+    def _compute_labels(
+        self, width, precision_list_train, precision_list_test, save_df=False
+    ):
+        train_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/train_features.pickle"
+        )
+        test_features = pd.read_pickle(
+            Path(settings.PROJECT_DIR) / "ioana/test_features.pickle"
+        )
         mean = float(self._calculate_precision_mean(precision_list_train))
         for features, precisions_list in [
-                (train_features, precision_list_train),
-                (test_features, precision_list_test),
-            ]:
+            (train_features, precision_list_train),
+            (test_features, precision_list_test),
+        ]:
             label_column = f"label_{width}"
             index_list = features.index.tolist()
             for i in range(len(features)):
@@ -146,40 +164,64 @@ class Labels:
                 else:
                     features.at[index_list[i], label_column] = 0
         if save_df:
-            train_path = f"/home/agriadapt/agriadapt/ioana/train_features_with_labels_width_{width}.pickle"
+            train_path = (
+                Path(settings.PROJECT_DIR)
+                / f"ioana/train_features_with_labels_width_{width}.pickle"
+            )
             train_features = train_features.to_pickle(train_path)
             print(f"Train data has been saved to path {train_path}")
 
-            test_path = f"/home/agriadapt/agriadapt/ioana/test_features_with_labels_width_{width}.pickle"
+            test_path = (
+                Path(settings.PROJECT_DIR)
+                / f"ioana/test_features_with_labels_width_{width}.pickle"
+            )
             test_features = test_features.to_pickle(test_path)
             print(f"Test data has been saved to {test_path}")
-        
+
         return train_features, test_features
-    
+
     def run(self):
+        models = {}
+        scalers = {}
         # self._load_data()
         for width, no_neighbors in settings.KNN_WIDTHS.items():
             self.train_loader, self.test_loader = self._filter_data()
             precision_list_train, precision_list_test = self._infer(width)
-            train_dataframe, test_dataframe = self._compute_labels(width, precision_list_train, precision_list_test)
+            train_dataframe, test_dataframe = self._compute_labels(
+                width, precision_list_train, precision_list_test
+            )
 
-            knn_model = KnnPrediction(width, train_dataframe, test_dataframe, no_neighbors)
+            knn_model = KnnPrediction(
+                width, train_dataframe, test_dataframe, no_neighbors
+            )
             X_train, y_train, X_test, y_test = knn_model.load_data()
-            X_train, X_test = knn_model.scale_data(X_train, X_test)
-            y_pred = knn_model.fit_model(X_train, y_train, X_test, y_test)
+            X_train, X_test, scaler = knn_model.scale_data(X_train, X_test)
+            y_pred = knn_model.fit_model(
+                X_train, y_train, X_test, y_test, save_model=True
+            )
 
             # finally, drop the lines that have label 1 in train_dataframe
-            train_dataframe = train_dataframe.drop(train_dataframe[train_dataframe[f'label_{width}'] == 1].index)
-            train_dataframe = train_dataframe.drop([f'label_{width}'], axis=1)
+            train_dataframe = train_dataframe.drop(
+                train_dataframe[train_dataframe[f"label_{width}"] == 1].index
+            )
+            train_dataframe = train_dataframe.drop([f"label_{width}"], axis=1)
 
-            test_dataframe = test_dataframe.drop(test_dataframe[test_dataframe[f'label_{width}'] == 1].index)
-            test_dataframe = test_dataframe.drop([f'label_{width}'], axis=1)
-            
+            test_dataframe = test_dataframe.drop(
+                test_dataframe[test_dataframe[f"label_{width}"] == 1].index
+            )
+            test_dataframe = test_dataframe.drop([f"label_{width}"], axis=1)
+
             self.train_features = train_dataframe
-            self.train_features.to_pickle(f"/home/agriadapt/agriadapt/ioana/train_features.pickle")
+            self.train_features.to_pickle(
+                Path(settings.PROJECT_DIR) / f"ioana/train_features.pickle"
+            )
             self.test_features = test_dataframe
-            self.test_features.to_pickle(f"/home/agriadapt/agriadapt/ioana/test_features.pickle")
-        
+            self.test_features.to_pickle(
+                Path(settings.PROJECT_DIR) / f"ioana/test_features.pickle"
+            )
+            models[width] = knn_model.model
+            scalers[width] = scaler
+        return models, scalers
 
 
 if __name__ == "__main__":
@@ -189,29 +231,75 @@ if __name__ == "__main__":
     train_images = ii.tensor_to_image(train.X)
     test_images = ii.tensor_to_image(test.X)
     image_lists = [train_images, test_images]
-    column_headers = ["index", "mean_brightness", "std_brightness", "max_brightness", "min_brightness",
-                    "no_bins", "contrast_hue_hist", "std_hue_arc", "contrast", "mean_saturation",
-                    "std_saturation", "max_saturation", "min_saturation", "keypoints", "ExG_ExR", 
-                    "CIVE_index","glcm_contrast_1","glcm_contrast_2","glcm_contrast_3","glcm_contrast_4", 
-                    "glcm_correlation_1","glcm_correlation_2", "glcm_correlation_3", "glcm_correlation_4",
-                    "glcm_dissimilarity_1","glcm_dissimilarity_2", "glcm_dissimilarity_3","glcm_dissimilarity_4",
-                    "glcm_asm_1", "glcm_asm_2", "glcm_asm_3", "glcm_asm_4",
-                    "glcm_energy_1", "glcm_energy_2", "glcm_energy_3", "glcm_energy_4",
-                    "glcm_homogeneity_1", "glcm_homogeneity_2", "glcm_homogeneity_3", "glcm_homogeneity_4"]
+    column_headers = [
+        "index",
+        "mean_brightness",
+        "std_brightness",
+        "max_brightness",
+        "min_brightness",
+        "no_bins",
+        "contrast_hue_hist",
+        "std_hue_arc",
+        "contrast",
+        "mean_saturation",
+        "std_saturation",
+        "max_saturation",
+        "min_saturation",
+        "keypoints",
+        "ExG_ExR",
+        "CIVE_index",
+        "glcm_contrast_1",
+        "glcm_contrast_2",
+        "glcm_contrast_3",
+        "glcm_contrast_4",
+        "glcm_correlation_1",
+        "glcm_correlation_2",
+        "glcm_correlation_3",
+        "glcm_correlation_4",
+        "glcm_dissimilarity_1",
+        "glcm_dissimilarity_2",
+        "glcm_dissimilarity_3",
+        "glcm_dissimilarity_4",
+        "glcm_asm_1",
+        "glcm_asm_2",
+        "glcm_asm_3",
+        "glcm_asm_4",
+        "glcm_energy_1",
+        "glcm_energy_2",
+        "glcm_energy_3",
+        "glcm_energy_4",
+        "glcm_homogeneity_1",
+        "glcm_homogeneity_2",
+        "glcm_homogeneity_3",
+        "glcm_homogeneity_4",
+    ]
 
     for list_index, image_list in enumerate(image_lists):
         list_of_dictionaries = []
         for i, image in enumerate(image_list):
-        
             spectral_features = SpectralFeatures(image)
             texture_features = TextureFeatures(image)
             vegetation_index = VegetationIndices(image)
 
             # color features
-            mean_brightness, std_brightness, max_brightness, min_brightness = spectral_features.compute_brightness()
-            no_bins, contrast_hue_hist, std_hue_arc = spectral_features.compute_hue_histogram()
+            (
+                mean_brightness,
+                std_brightness,
+                max_brightness,
+                min_brightness,
+            ) = spectral_features.compute_brightness()
+            (
+                no_bins,
+                contrast_hue_hist,
+                std_hue_arc,
+            ) = spectral_features.compute_hue_histogram()
             contrast = spectral_features.compute_contrast()
-            mean_saturation, std_saturation, max_saturation, min_saturation = spectral_features.compute_saturation()
+            (
+                mean_saturation,
+                std_saturation,
+                max_saturation,
+                min_saturation,
+            ) = spectral_features.compute_saturation()
             keypoints = spectral_features.compute_sift_feats()
 
             # vegetation indices
@@ -219,19 +307,27 @@ if __name__ == "__main__":
             ExR = vegetation_index.excess_red_index()
             ExG_ExR_img = vegetation_index.excess_green_excess_red_index(ExG, ExR)
             CIVE_index = vegetation_index.colour_index_vegetation_extraction()
-            binary_CIVE_image = vegetation_index.visualization_CIVE_Otsu_threshold(CIVE_index)
+            binary_CIVE_image = vegetation_index.visualization_CIVE_Otsu_threshold(
+                CIVE_index
+            )
 
             # texture features
             glcm_matrix = texture_features.compute_glcm()
             glcm_contrast = np.ravel(texture_features.contrast_feature(glcm_matrix))
-            glcm_dissimilarity = np.ravel(texture_features.dissimilarity_feature(glcm_matrix))
-            glcm_homogeneity = np.ravel(texture_features.homogeneity_feature(glcm_matrix))
+            glcm_dissimilarity = np.ravel(
+                texture_features.dissimilarity_feature(glcm_matrix)
+            )
+            glcm_homogeneity = np.ravel(
+                texture_features.homogeneity_feature(glcm_matrix)
+            )
             glcm_energy = np.ravel(texture_features.energy_feature(glcm_matrix))
-            glcm_correlation = np.ravel(texture_features.correlation_feature(glcm_matrix))
+            glcm_correlation = np.ravel(
+                texture_features.correlation_feature(glcm_matrix)
+            )
             glcm_asm = np.ravel(texture_features.asm_feature(glcm_matrix))
-            
+
             features_dictionary = {
-                "index" : i,
+                "index": i,
                 "mean_brightness": mean_brightness,
                 "std_brightness": std_brightness,
                 "max_brightness": max_brightness,
@@ -274,21 +370,23 @@ if __name__ == "__main__":
             }
 
             list_of_dictionaries.append(features_dictionary)
-        
-        if list_index == 0:
-            train_features = pd.DataFrame(list_of_dictionaries, columns=column_headers, index=None)
-        else:
-            test_features = pd.DataFrame(list_of_dictionaries, columns=column_headers, index=None)
 
-    file_name = '/home/agriadapt/agriadapt/ioana/train_features.pickle'
+        if list_index == 0:
+            train_features = pd.DataFrame(
+                list_of_dictionaries, columns=column_headers, index=None
+            )
+        else:
+            test_features = pd.DataFrame(
+                list_of_dictionaries, columns=column_headers, index=None
+            )
+
+    file_name = Path(settings.PROJECT_DIR) / "ioana/train_features.pickle"
     train_features.to_pickle(file_name)
     print(f"Training features saved to {file_name}")
 
-    file_name = '/home/agriadapt/agriadapt/ioana/test_features.pickle'
+    file_name = Path(settings.PROJECT_DIR) / "ioana/test_features.pickle"
     test_features.to_pickle(file_name)
     print(f"Testing features saved to {file_name}")
 
-    labels = Labels(
-        "cofly_slim_256.pt"
-    )
+    labels = Labels("cofly_slim_256.pt")
     labels.run()