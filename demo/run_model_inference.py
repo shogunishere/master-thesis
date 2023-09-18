0ab7ff16c56328ecee7256b69de559b898fb1212
KRASOVEC Andraz <andraz.krasovec@ext.ec.europa.eu>
2023-09-18 11:01:19 +0200

    Width distribution visualisation implemented.

--- demo/run_model_inference.py
+++ demo/run_model_inference.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
+from plotly import graph_objects as go
 from torch.utils.data import DataLoader
 from torchmetrics.classification import (
     BinaryPrecision,
@@ -37,7 +38,6 @@ class Inference:
         metrics=None,
         image_resolution=None,
         create_images=False,
-        mode="sweep",
     ):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.model_name = model.split(".")[0]
@@ -55,6 +55,7 @@ class Inference:
             }
             for x in settings.WIDTHS + ["adapt"]
         }
+        self.width_distribution = {x: 0 for x in settings.WIDTHS}
         self.width_selection = AdaptiveWidth()
         self.tensor_to_image = ImageImporter("cofly").tensor_to_image
 
@@ -114,6 +115,7 @@ class Inference:
                 image = self.tensor_to_image(X)[0]
                 width = self.width_selection.get_image_width(image)
                 self.model.set_width(width)
+                self.width_distribution[width] += 1
                 # Resize the image to the required input size
                 X = smaller(X)
                 y = smaller(y)
@@ -167,10 +169,9 @@ class Inference:
 
 
 class Comparator:
-    def __init__(self, models, metrics=None, mode="sweep"):
+    def __init__(self, models, metrics=None):
         self.models = models
         self.metrics = METRICS if metrics is None else metrics
-        self.mode = mode
 
     def _draw_tab(self, results):
         for pred_class in ["back", "weeds"]:
@@ -206,15 +207,33 @@ class Comparator:
         # Select a model from andraz/training/garage directory and set the
         # image resolution tuple to match the image input size of the model
         results = {}
+        graph = True
         for model, size in self.models:
             infer = Inference(
                 model,
                 image_resolution=(size, size),
                 create_images=False,
-                mode=self.mode,
             )
             infer.run()
             results[model] = infer.results
+
+            if graph:
+                x = [k for k in infer.width_distribution]
+                y = [v for _, v in infer.width_distribution.items()]
+                fig = go.Figure(
+                    data=go.Bar(
+                        x=x,
+                        y=y,
+                    )
+                )
+                fig.update_layout(
+                    {"title": "Distribution of selected widths for test images."}
+                )
+                fig.update_xaxes(type="category")
+                fig.show()
+                # Only show once as all the plots will be the same
+                graph = False
+
         self._draw_tab(results)
 
 
@@ -227,7 +246,7 @@ if __name__ == "__main__":
     models = [("cofly_slim_{}.pt".format(size), size) for size in [128, 256, 512]] + [
         ("cofly_squeeze_{}.pt".format(size), size) for size in [128, 256, 512]
     ]
-    comparator = Comparator(models, mode="adaptive")
+    comparator = Comparator(models)
 
     # Run inference for multiple models and display comparative tables
     comparator.run()
