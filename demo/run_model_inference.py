Bugfix of metric calculation discrepancy between the fixed width and adaptation approaches.

demo/run_model_inference.py
@@ -80,13 +80,18 @@ class Inference:
         return DataLoader(test, batch_size=1, shuffle=False)
 
     def _infer(self):
+        smaller = transforms.Resize(self.image_resolution)
+        test_loader = self._load_test_data(self.image_resolution)
         # Infer for all available widths of the model
         with torch.no_grad():
-            test_loader = self._load_test_data(self.image_resolution)
+
             for width in settings.WIDTHS:
                 self.model.set_width(width)
                 i = 1
                 for X, y in test_loader:
+                    # Resize the image to the required input size
+                    X = smaller(X)
+                    y = smaller(y)
                     X = X.to("cuda:0")
                     y = y.to("cuda:0")
                     y_pred = self.model.forward(X)
@@ -113,8 +118,6 @@ class Inference:
         with torch.no_grad():
             # We load the test loader again, as we need full-size
             # images in order for the width selection to work properly
-            test_loader = self._load_test_data(False)
-            smaller = transforms.Resize(self.image_resolution)
             i = 1
             for X, y in test_loader:
                 # Get the image width and set the model to it
@@ -126,7 +129,6 @@ class Inference:
                 # Resize the image to the required input size
                 X = smaller(X)
                 y = smaller(y)
-
                 X = X.to("cuda:0")
                 y = y.to("cuda:0")
                 y_pred = self.model.forward(X)